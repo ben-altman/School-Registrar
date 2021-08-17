50fa84f3fef3b337940e6faea40f957f03bd3567
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-08-17 00:17:43 -0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-
+  root 'sessions#welcome'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   get '/signup', to: 'teachers#new'
-  post 'signup', to: 'teachers#create'
-  get 'teachers/:id', to 'teachers#show', as: 'teacher'
+  post '/signup', to: 'teachers#create'
+  get '/teachers/:id', to 'teachers#show' as: 'teacher'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
