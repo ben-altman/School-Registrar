9307a4b5faf36151ab9c3b4f8d32ae884f1ef07f
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-08-16 21:51:41 -0400

    creates teachers controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+
+  
+  get '/signup', to: 'teachers#new'
+  post 'signup', to: 'teachers#create'
+  get 'teachers/:id', to 'teachers#show', as: 'teacher'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
