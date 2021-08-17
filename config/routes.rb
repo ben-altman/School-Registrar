789a670e8a4bcad7a8594c1cb0d5b90ad795989e
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-08-16 21:56:05 -0400

    sessions routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
 
-  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   get '/signup', to: 'teachers#new'
   post 'signup', to: 'teachers#create'
   get 'teachers/:id', to 'teachers#show', as: 'teacher'
