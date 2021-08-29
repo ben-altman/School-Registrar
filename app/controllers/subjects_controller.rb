73dd5c4700e7b704eaa6a803322e84dbeac7d901
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-08-28 23:33:50 -0400

    updates spec.md

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,6 @@
 class SubjectsController < ApplicationController
+
+
+
+    
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,7 @@
 class Subject < ApplicationRecord
     validates :name, presence: :true
-
+    validates :name, uniqueness: :true
+    
     has_many :courses
     has_many :teachers, through: :courses
 end
