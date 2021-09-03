7283959cd127d5bff050c37635e82d4116b7fb56
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-09-02 22:14:32 -0400

    adds helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+    helper_method :current_user, logged_in?
+
+    def logged_in?
+        !!session[:teacher_id]
+    end
 
     def current_user
         @teacher = Teacher.find_by(id: session[:teacher_id])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+    def display_nav_bar
+        if logged_in?
+            render partial: 'layouts/navbar_links'
+        end
+    end
 end
